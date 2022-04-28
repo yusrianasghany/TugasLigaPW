49b11aedbaf45deba2ce98b1b20545cabbfc3410
puskipus <95998981+puskipus@users.noreply.github.com>
2022-04-28 15:59:37 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sepak Bola Liga PW</title>
+</head>
+<body>
+    <h1>Silakan pilih apa yang Anda ingin lakukan</h1>
+
+    <ul>
+        <li><a href="input.php">Input data klub sepak bola</a></li>
+        <li><a href="">Input jadwal pertandingan sepak bola</a></li>
+        <li><a href="">Input hasil skor pertandingan sepak bola</a></li>
+        <li><a href="">Menampilkan peringkat klasemen sementara</a></li>
+    </ul>
+    
+
+</body>
+</html>
---
+++ input.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Input Klub</title>
+</head>
+<body>
+
+<h1>Masukkan data klub sepak bola</h1>
+
+<form action="" method="post">
+    Nama Klub: <input type="text" name="nama">
+    <br>
+    
+</form>
+    
+</body>
+</html>
