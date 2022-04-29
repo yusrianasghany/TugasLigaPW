d92dc1cf06af0051f2838900fed2f9b140c96e0c
puskipus <95998981+puskipus@users.noreply.github.com>
2022-04-29 20:21:13 +0700

    Update connectMVC.php

--- connectMVC.php
+++ connectMVC.php
@@ -27,16 +27,42 @@ function gol($data) {
     $gol2 = htmlspecialchars($data["gol2"]);
     $lawan1 = htmlspecialchars($data["lawan1"]);
     $lawan2 = htmlspecialchars($data["lawan2"]);
-    // addGol($lawan1, $gol1);
-    // addGol($lawan2, $gol2);
+    $totalGol1 = (int) "SELECT jumlahGol FROM club WHERE namaClub='$lawan1'" + $gol1;
+    $query1 = "UPDATE club SET jumlahGol='$totalGol1' WHERE namaClub='$lawan1'";
+    mysqli_query($conn, $query1);
 
+    $totalGol2 = (int) "SELECT jumlahGol FROM club WHERE namaClub='$lawan2'" + $gol2;
+    $query2 = "UPDATE club SET jumlahGol='$totalGol2' WHERE namaClub='$lawan2'";
+    mysqli_query($conn, $query2);
+
+    if($gol1 > $gol2) {
+        $skorTambah = (int) "SELECT jumlahSkor FROM club WHERE namaClub='$lawan1'" + 3;
+        $query3 = "UPDATE club SET jumlahSkor='$skorTambah' WHERE namaClub='$lawan1'";
+        mysqli_query($conn, $query3);
+    } else if ($gol1 == $gol2) {
+        $skorTambah1 = (int) "SELECT jumlahSkor FROM club WHERE namaClub='$lawan1'" + 1;
+        $skorTambah2 = (int) "SELECT jumlahSkor FROM club WHERE namaClub='$lawan2'" + 1;
+        $query3 = "UPDATE club SET jumlahSkor='$skorTambah1' WHERE namaClub='$lawan1'";
+        $query4 = "UPDATE club SET jumlahSkor='$skorTambah2' WHERE namaClub='$lawan2'";
+        mysqli_query($conn, $query3);
+        mysqli_query($conn, $query4);
+    } else {
+        $skorTambah = (int) "SELECT jumlahSkor FROM club WHERE namaClub='$lawan2'" + 3;
+        $query3 = "UPDATE club SET jumlahSkor='$skorTambah' WHERE namaClub='$lawan2'";
+        mysqli_query($conn, $query3);
+    }
+
+    return mysqli_affected_rows($conn);
 }
 
-function addGol($namaklub, $gol){
-    global $conn;
-    $totalGol = ($conn->query("SELECT jumlahGol FROM club WHERE namaClub='$namaklub'")) + $gol;
-    $rs = $conn->query("UPDATE club SET jumlahGol='$totalGol' WHERE namaClub='$namaklub'");
-}
+
+
+// function addGol($namaklub, $gol){
+//     global $conn;
+//     $totalGol = "SELECT jumlahGol FROM club WHERE namaClub='$namaklub'" + $gol;
+//     $query = "UPDATE club SET jumlahGol=$totalGol WHERE namaClub=$namaklub";
+//     mysqli_query($conn, $query);
+// }
 
 function add($data) {
     global $conn;
