71776d39dae3f42636df4b1fbc8693c403ed7ab9
puskipus <95998981+puskipus@users.noreply.github.com>
2022-04-29 08:41:27 +0700

    Update connectMVC.php

--- connectMVC.php
+++ connectMVC.php
@@ -1,2 +1,11 @@
 <?php
-    $mysqli = new mysqli('localhost', 'root', '', '');
+    $conn = new mysqli('localhost', 'root', '', 'ligapw');
+
+function add($data) {
+    global $conn;
+    $nama = $data["nama"];
+    $query = "INSERT INTO club VALUES ('', '$nama', 0, 0)";
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
