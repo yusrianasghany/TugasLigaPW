e4492edb9c0e6508cc79927c9978aec47166d539
larasatisz <100670294+larasatisz@users.noreply.github.com>
2022-04-30 08:13:21 +0700

    Update juara.php

--- juara.php
+++ juara.php
@@ -11,13 +11,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styleJuara.css">
     <title>Klub Juara</title>
 </head>
 <body>
 
     <h1>Juara Liga PW</h1>
 
-    <table border="1" cellpadding="10" cellspacing="0">
+    <table border="1" cellpadding="10" cellspacing="0" align=center>
         <tr>
             <th>Nama Klub</th>
             <th>Jumlah Skor</th>
@@ -34,10 +35,12 @@
     </table>
 
     <h1>Selamat kepada Klub <?= $row["namaClub"]; ?></h1>
-    <br>
-    <input type="button" value="Go Back" onclick="history.back(-1)" />
-
-
     
+    <br> <br>
+
+    <div class="button">
+    <input type="button" value="Go Back" onclick="history.back(-1)" />
+    </div>
+
 </body>
 </html>
