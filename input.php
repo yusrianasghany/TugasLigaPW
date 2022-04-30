2a7f29669b2bb22c6770789bd4f1b6138d83305c
larasatisz <100670294+larasatisz@users.noreply.github.com>
2022-04-30 07:12:26 +0700

    Update input.php

--- input.php
+++ input.php
@@ -25,18 +25,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styleInput.css">
     <title>Input Klub</title>
 </head>
 <body>
-
 <h1>Masukkan data klub sepak bola</h1>
 
 <form action="" method="post">
-    <label for="nama">Nama Klub</label>
+<br> 
+<br> 
+    <label for="nama">Nama Klub </label>
     <input type="text" name="nama" id="nama" required>
     <br>
     <button type="submit" name="submit">Add</button>
     <input type="button" value="Go Back" onclick="history.back(-1)" />
+
 </form>
     
 </body>
