e4ea68d89a098f48bdd3a97215b28bbfb4999dcf
larasatisz <100670294+larasatisz@users.noreply.github.com>
2022-04-30 07:19:49 +0700

    Update hasil.php

--- hasil.php
+++ hasil.php
@@ -27,6 +27,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet href=styleHasil.css> 
     <title>Input Skor pertandingan</title>
 </head>
 <body>
